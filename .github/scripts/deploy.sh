79ef31a78849f4be43181f3ac5be78b811b15740
pb-coding <71174645+pb-coding@users.noreply.github.com>
2023-08-29 17:00:20 +0200

    wip

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -6,9 +6,8 @@ DOCKER_REGISTRY="ghcr.io"
 DOCKER_USERNAME="pb-coding"
 DOCKER_IMAGE_NAME="voltvector-be"
 CONTAINER_NAME="voltvector-be"
-TARGET_DIRECTORY="/var/www/$CONTAINER_NAME"
-DOCKER_NETWORK="swag_net"
-FIXED_IP="172.18.0.70"
+TARGET_DIRECTORY="/home/pb1497/deployments/$CONTAINER_NAME"
+DOCKER_COMPOSE_FILE="docker-compose.yml"
 
 # Authenticate with Docker registry
 echo "Authenticating with Docker registry..."
@@ -18,20 +17,9 @@ docker login -u $DOCKER_USERNAME --password $GITHUB_TOKEN $DOCKER_REGISTRY
 echo "Pulling the latest Docker image..."
 docker pull $DOCKER_REGISTRY/$DOCKER_USERNAME/$DOCKER_IMAGE_NAME:latest
 
-# Stop and remove the existing container if it exists
-if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-    echo "Stopping and removing the existing container..."
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
-fi
-
-# Run the new Docker container
-echo "Starting a new container from the latest image..."
-docker run -d \
-  --name $CONTAINER_NAME \
-  --network $DOCKER_NETWORK \
-  --ip $FIXED_IP \
-  -v $TARGET_DIRECTORY:/app \
-  $DOCKER_REGISTRY/$DOCKER_USERNAME/$DOCKER_IMAGE_NAME:latest
+cd $TARGET_DIRECTORY
+docker-compose down
+docker-compose pull
+docker-compose up -d
 
 echo "Deployment complete."
