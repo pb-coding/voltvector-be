16b58438c880e75fd016a4a175bfc5db28caf1df
pb-coding <71174645+pb-coding@users.noreply.github.com>
2023-09-01 17:35:18 +0200

    wip

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "https://localhost:3001"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://localhost:3001",
+  "https://voltvector.org",
+];
 
 export default allowedOrigins;
