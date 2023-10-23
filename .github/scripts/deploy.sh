2a2e0a267713182f61fe7af0403fafcade111f79
Philipp Beckmann <71174645+pb-coding@users.noreply.github.com>
2023-10-23 23:22:51 +0200

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -8,16 +8,19 @@ DOCKER_IMAGE_NAME="voltvector-be"
 CONTAINER_NAME="voltvector-be"
 TARGET_DIRECTORY="/home/pb1497/deployments/$CONTAINER_NAME"
 DOCKER_COMPOSE_FILE="docker-compose.yml"
+DOCKER_REGISTRY_TOKEN=$1
 
 # Authenticate with Docker registry
 echo "Authenticating with Docker registry..."
-docker login -u $DOCKER_USERNAME --password $GITHUB_TOKEN $DOCKER_REGISTRY
+docker login -u $DOCKER_USERNAME --password $DOCKER_REGISTRY_TOKEN $DOCKER_REGISTRY
 
 # Pull the latest Docker image
 echo "Pulling the latest Docker image..."
 docker pull $DOCKER_REGISTRY/$DOCKER_USERNAME/$DOCKER_IMAGE_NAME:latest
 
+mkdir -p $TARGET_DIRECTORY
 cd $TARGET_DIRECTORY
+
 docker-compose down
 docker-compose pull
 docker-compose up -d
